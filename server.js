dafc15be4922af5b331fc73158b15532ed7fe522
abdulwahab100 <abdulwahhab.aw1@gmail.com>
2020-04-25 16:17:02 +0500

    y

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const users = require('./route/api/users');
 const auth = require('./route/api/auth');
 var cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(express.static('public'));
 
@@ -17,11 +19,13 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
